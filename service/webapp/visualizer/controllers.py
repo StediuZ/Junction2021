f5fd5465be1259033566efc16c3f7a55bfbc3118
54553 <55926848+54553@users.noreply.github.com>
2021-11-20 15:48:35 +0200

    Updated url routes and added static site pictures

--- service/webapp/visualizer/controllers.py
+++ service/webapp/visualizer/controllers.py
@@ -1,11 +1,13 @@
-from flask import render_template, Blueprint, flash, redirect, url_for, current_app, abort, request
+from flask import render_template, Blueprint, flash, redirect, url_for, current_app, abort, request, send_from_directory
 from .models import TimeNow
 
 visualizer_blueprint = Blueprint(
     'visualizer',
     __name__,
     template_folder='../templates/visualizer',
-    url_prefix="/visualizer"
+    url_prefix="/visualizer",
+    static_url_path='',
+    static_folder='../static',
 )
 
 
@@ -13,10 +15,22 @@ visualizer_blueprint = Blueprint(
 def home():
     return render_template('home.html')
 
-@visualizer_blueprint.route('/date', methods=['GET', 'POST'])
-def querydate():
+
+@visualizer_blueprint.route('/', methods=['GET', 'POST'])
+def submitdate():
     dateSelected = request.form['date']
     return dateSelected
+
+
+@visualizer_blueprint.route('/date/<dateSelected>')
+def urldate(dateSelected):
+    return dateSelected
+
+
+@visualizer_blueprint.route('/site/<int:num>')
+def sitepicture(num):
+    siteName = 'site_' + str(num) + '.png'
+    return send_from_directory('static', siteName)
 
 
 @visualizer_blueprint.route('/time')
